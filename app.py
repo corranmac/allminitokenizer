b7fba15615a8e06b778c569674175f6fa5088bc9
corranmac <corranmac@hotmail.co.uk>
2022-11-19 17:11:05 +0000

    Update to marco allmini

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from transformers import pipeline
+from transformers import AutoTokenizer, AutoModel
 import torch
+import torch.nn.functional as F
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
@@ -7,20 +8,29 @@ def init():
     global model
     
     device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+    model = AutoModel.from_pretrained('sentence-transformers/all-distilroberta-v1')
+    tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/all-distilroberta-v1')
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
 def inference(model_inputs:dict) -> dict:
     global model
+    global tokenizer
 
     # Parse out your arguments
-    prompt = model_inputs.get('prompt', None)
+    sentences = model_inputs.get('prompts', None)
     if prompt == None:
         return {'message': "No prompt provided"}
-    
-    # Run the model
-    result = model(prompt)
+    encoded_input = tokenizer(sentences, padding=True, truncation=True, return_tensors='pt')
+
+    with torch.no_grad():
+        model_output = model(**encoded_input)
+
+    # Perform pooling
+    sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
+
+    # Normalize embeddings
+    sentence_embeddings = F.normalize(sentence_embeddings, p=2, dim=1)
 
     # Return the results as a dictionary
-    return result
+    return sentence_embeddings
